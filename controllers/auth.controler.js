6767557fb706f7389044d8ad726c4ca452242a95
Salman Rahman <md.salman@cefalo.com>
2025-10-24 13:18:18 +0600

    implement user authentication with sign up and sign in functionality

---
+++ controllers/auth.controler.js
@@ -0,0 +1,89 @@
+import mongoose from 'mongoose';
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET, JWT_EXPIRES_IN } from '../config/env.js';
+
+export const signUp = async (req, res, next) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const { name, email, password } = req.body;
+
+    // check if user already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      const error = new Error('User with this email already exists');
+      error.status = 409;
+      throw error;
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUsers = await User.create([{ name, email, password: hashedPassword }], { session });
+
+    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({
+      success: true,
+      message: 'User created successfully',
+      data: {
+        token,
+        user: newUsers[0],
+      },
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    next(error);
+  } finally {
+    session.endSession();
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user by email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      const error = new Error('Invalid email or password');
+      error.status = 401;
+      throw error;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      const error = new Error('Invalid email or password');
+      error.status = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: 'User signed in successfully',
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// export const signOut = (req, res, next) => {};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+import { signIn, signUp } from '../controllers/auth.controler.js';
 
 const authRouter = Router();
 
-authRouter.post('sign-up', (req, res) => res.send({ title: 'Sign Up' }));
+authRouter.post('/sign-up', signUp);
 
-authRouter.post('sign-in', (req, res) => res.send({ title: 'Sign In' }));
+authRouter.post('/sign-in', signIn);
 
-authRouter.post('sign-out', (req, res) => res.send({ title: 'Sign Out' }));
+authRouter.post('/sign-out', () => {});
 
 export default authRouter;
